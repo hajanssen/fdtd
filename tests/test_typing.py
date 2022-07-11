901a17dbcddea42b27bc3b2011ff87f098854bb5
Hauke Janssen <45470500+hajanssen@users.noreply.github.com>
2022-07-11 21:56:08 +0200

    added test for types in LineDetector

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -3,6 +3,43 @@
 
 ## Imports
 import fdtd
-import pytest
+
+# dict to test the arrray class for the backends
+class_for_backend = {
+    "numpy": "numpy.ndarray",
+    "torch.float32": "torch.Tensor",
+    "torch.float64": "torch.Tensor",
+    "torch.cuda.float32": "torch.Tensor",
+    "torch.cuda.float64": "torch.Tensor",
+}
+
+# dict to test the dtype from the backends
+dtype_for_backend = {
+    "numpy": "float64",
+    "torch.float32": "torch.float32",
+    "torch.float64": "torch.float64",
+    "torch.cuda.float32": "torch.float32",
+    "torch.cuda.float64": "torch.float64",
+}
 
 ## Tests
+def test_class_of_saved_data_from_LineDetector_all_bends(backends):
+    fdtd.set_backend(backends)
+    grid = fdtd.Grid(shape=(3, 3, 1))
+    grid[2, 2] = fdtd.detectors.LineDetector()
+    grid.run(2)
+
+    var_to_test = grid.detectors[0].E[0]
+
+    assert str(type(var_to_test)) == "<class '" + class_for_backend[backends] + "'>"
+
+
+def test_dtype_of_saved_data_from_LineDetector_all_bends(backends):
+    fdtd.set_backend(backends)
+    grid = fdtd.Grid(shape=(3, 3, 1))
+    grid[2, 2] = fdtd.detectors.LineDetector()
+    grid.run(2)
+
+    var_to_test = grid.detectors[0].E[0]
+    
+    assert str(var_to_test.dtype) == dtype_for_backend[backends]
